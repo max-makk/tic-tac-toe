7148b6d1a0ae2ae2e2a28ef547af042d51661b50
maxmakk <90equals50@gmail.com>
2022-01-27 14:44:47 +0200

    minor edits

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const Player = (name, mark) => {
-  const setMark = (m) => (mark = m);
-  const setName = (n) => (name = n);
-  const getName = () => name;
-  const getMark = () => mark;
-  return { setName, getMark, setMark, getName };
-};
+  const setMark = (m) => (mark = m)
+  const setName = (n) => (name = n)
+  const getName = () => name
+  const getMark = () => mark
+  return { setName, getMark, setMark, getName }
+}
 
 const gameBoard = (() => {
-  const board = new Array(9).fill("-");
-  const addMark = (i, m) => (board[i] = m);
+  const board = new Array(9).fill('-')
+  const addMark = (i, m) => (board[i] = m)
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,204 +18,203 @@ const gameBoard = (() => {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+  ]
 
   const checkBoard = (b) => {
-    let result = false;
-    const cur = displayController.getCurrent();
-    if (!b.includes("-")) {
-      result = "tie";
+    let result = false
+    const current = displayController.getCurrent()
+    if (!b.includes('-')) {
+      result = 'tie'
     }
     winConditions.forEach((el) => {
-      if (b[el[0]] === cur && b[el[1]] === cur && b[el[2]] === cur) {
-        result = cur;
+      if (
+        b[el[0]] === current &&
+        b[el[1]] === current &&
+        b[el[2]] === current
+      ) {
+        result = current
       }
-    });
-    return result;
-  };
-  const getBoard = () => board;
-  const clearBoard = () => board.fill("-");
+    })
+    return result
+  }
+  const getBoard = () => board
+  const clearBoard = () => board.fill('-')
   return {
     checkBoard,
     addMark,
     getBoard,
     clearBoard,
-  };
-})();
+  }
+})()
 
 const displayController = (() => {
-  const start = document.querySelector(".start");
-  const name1 = document.querySelector("#player-1");
-  const name2 = document.querySelector("#player-2");
-  const xo = document.querySelector("#switch");
-  const displayAlert = document.querySelector(".display-alert");
-  const alertMessage = document.querySelector(".alert-text");
-  const ok = document.querySelector(".ok");
-  const overlay = document.querySelector(".overlay");
+  const cells = document.querySelectorAll('td')
+  const start = document.querySelector('.start')
+  const name1 = document.querySelector('#player-1')
+  const name2 = document.querySelector('#player-2')
+  const xo = document.querySelector('#switch')
+  const displayAlert = document.querySelector('.display-alert')
+  const alertMessage = document.querySelector('.alert-text')
+  const ok = document.querySelector('.ok')
+  const overlay = document.querySelector('.overlay')
 
-  let ai = false;
-  let gameStarted = false;
-  let cur = "?";
+  let ai = false
+  let gameStarted = false
+  let current = '?'
 
-  const leftPlayer = Player(name1.value, "?");
-  const rightPlayer = Player(name2.value, "?");
+  const leftPlayer = Player(name1.value, '?')
+  const rightPlayer = Player(name2.value, '?')
 
-  start.addEventListener("click", function () {
-    cur = "o";
+  start.addEventListener('click', function () {
+    current = 'o'
     if (xo.checked) {
-      cur = "x";
+      current = 'x'
     }
-    clearCells();
-    leftPlayer.setName(name1.value);
-    rightPlayer.setName(name2.value);
-    leftPlayer.setMark(cur === "o" ? "x" : "o");
-    rightPlayer.setMark(cur === "x" ? "o" : "x");
-    gameStarted = true;
-    xo.disabled = true;
-    name1.disabled = true;
-    name2.disabled = true;
+    clearCells()
+    leftPlayer.setName(name1.value)
+    rightPlayer.setName(name2.value)
+    leftPlayer.setMark(current === 'o' ? 'x' : 'o')
+    rightPlayer.setMark(current === 'x' ? 'o' : 'x')
+    gameStarted = true
+    xo.disabled = true
+    name1.disabled = true
+    name2.disabled = true
     if (
-      leftPlayer.getName().toLowerCase() === "ai" ||
-      rightPlayer.getName().toLowerCase() === "ai"
+      leftPlayer.getName().toLowerCase() === 'ai' ||
+      rightPlayer.getName().toLowerCase() === 'ai'
     ) {
-      if (leftPlayer.getName().toLowerCase() === "ai") {
-        cur = cur === "x" ? "o" : "x";
-        const id = aiMove.findBestMove(gameBoard.getBoard());
-        gameBoard.addMark(+id, cur);
-        setTimeout(() => {
-          document.getElementById("" + id).classList.add(cur);
-        }, 100);
+      if (leftPlayer.getName().toLowerCase() === 'ai') {
+        aiMove()
       }
-      ai = true;
+      ai = true
     } else {
-      ai = false;
+      ai = false
     }
-  });
+  })
 
-  const cells = document.querySelectorAll("td");
   cells.forEach((el) => {
-    el.addEventListener("click", (e) => {
-      if (!gameStarted) return;
-      if (gameBoard.getBoard()[+e.target.id] !== "-") {
-        return;
+    el.addEventListener('click', (e) => {
+      if (!gameStarted) return
+      if (gameBoard.getBoard()[+e.target.id] !== '-') {
+        return
       }
-      if (cur === "x") {
-        gameBoard.addMark(+e.target.id, "o");
-        e.target.classList.add("o");
-        cur = "o";
+      if (current === 'x') {
+        gameBoard.addMark(+e.target.id, 'o')
+        e.target.classList.add('o')
+        current = 'o'
         if (checkForWinner()) {
-          return;
+          return
         }
         if (ai) {
-          cur = cur === "x" ? "o" : "x";
-          const id = aiMove.findBestMove(gameBoard.getBoard());
-          gameBoard.addMark(+id, cur);
-          setTimeout(() => {
-            document.getElementById("" + id).classList.add(cur);
-          }, 100);
+          aiMove()
           if (checkForWinner()) {
-            return;
+            return
           }
         }
       } else {
-        gameBoard.addMark(+e.target.id, "x");
-        e.target.classList.add("x");
-        cur = "x";
+        gameBoard.addMark(+e.target.id, 'x')
+        e.target.classList.add('x')
+        current = 'x'
         if (checkForWinner()) {
-          return;
+          return
         }
         if (ai) {
-          cur = cur === "x" ? "o" : "x";
-          const id = aiMove.findBestMove(gameBoard.getBoard());
-          gameBoard.addMark(+id, cur);
-          setTimeout(() => {
-            document.getElementById("" + id).classList.add(cur);
-          }, 100);
+          aiMove()
           if (checkForWinner()) {
-            return;
+            return
           }
         }
       }
-    });
-  });
+    })
+  })
+
+  function aiMove() {
+    current = current === 'x' ? 'o' : 'x'
+    const id = optimalMove.findBestMove(gameBoard.getBoard())
+    gameBoard.addMark(+id, current)
+    setTimeout(() => {
+      document.getElementById('' + id).classList.add(current)
+    }, 100)
+  }
 
   function checkForWinner() {
-    const result = gameBoard.checkBoard(gameBoard.getBoard());
-    if (result === cur) {
-      if (leftPlayer.getMark() === cur) {
-        alertMessage.textContent = leftPlayer.getName() + " WINS";
+    const result = gameBoard.checkBoard(gameBoard.getBoard())
+    if (result === current) {
+      if (leftPlayer.getMark() === current) {
+        alertMessage.textContent = leftPlayer.getName() + ' WINS'
       } else {
-        alertMessage.textContent = rightPlayer.getName() + " WINS";
+        alertMessage.textContent = rightPlayer.getName() + ' WINS'
       }
-      displayAlert.classList.toggle("closed");
-      overlay.classList.toggle("closed");
-      return true;
-    } else if (result === "tie") {
-      alertMessage.textContent = "TIE";
-      displayAlert.classList.toggle("closed");
-      overlay.classList.toggle("closed");
-      return true;
+      displayAlert.classList.toggle('closed')
+      overlay.classList.toggle('closed')
+      return true
+    } else if (result === 'tie') {
+      alertMessage.textContent = 'TIE'
+      displayAlert.classList.toggle('closed')
+      overlay.classList.toggle('closed')
+      return true
     }
   }
 
-  ok.addEventListener("click", function () {
-    clearCells();
-    gameStarted = false;
-    xo.disabled = false;
-    name1.disabled = false;
-    name2.disabled = false;
-    displayAlert.classList.toggle("closed");
-    overlay.classList.toggle("closed");
-  });
+  ok.addEventListener('click', function () {
+    clearCells()
+    gameStarted = false
+    xo.disabled = false
+    name1.disabled = false
+    name2.disabled = false
+    displayAlert.classList.toggle('closed')
+    overlay.classList.toggle('closed')
+  })
+
   function clearCells() {
     cells.forEach((el) => {
-      el.classList.remove("x");
-      el.classList.remove("o");
-    });
-    gameBoard.clearBoard();
+      el.classList.remove('x')
+      el.classList.remove('o')
+    })
+    gameBoard.clearBoard()
   }
   alertMessage.textContent =
-    "Enter your name or play agains the computer (enter AI)";
-  displayAlert.classList.toggle("closed");
-  overlay.classList.toggle("closed");
+    "To play against the computer, enter 'AI' in one of the input fields."
+  displayAlert.classList.toggle('closed')
+  overlay.classList.toggle('closed')
 
-  const getCurrent = () => cur;
+  const getCurrent = () => current
   return {
     leftPlayer,
     rightPlayer,
     getCurrent,
-  };
-})();
+  }
+})()
 
-const aiMove = (() => {
+const optimalMove = (() => {
   function isMovesLeft(board) {
-    for (var i = 0; i < board.length; i++) {
-      if (board[i] == "-") {
-        return true;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == '-') {
+        return true
       }
     }
-    return false;
+    return false
   }
 
   function evaluate() {
-    const player = displayController.getCurrent();
-    const opponent = displayController.getCurrent() === "x" ? "o" : "x";
-    const board = gameBoard.getBoard();
-    for (var i = 0; i < board.length; i += 3) {
+    const player = displayController.getCurrent()
+    const opponent = displayController.getCurrent() === 'x' ? 'o' : 'x'
+    const board = gameBoard.getBoard()
+    for (let i = 0; i < board.length; i += 3) {
       if (board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
         if (board[i] == player) {
-          return +10;
+          return +10
         } else if (board[i] == opponent) {
-          return -10;
+          return -10
         }
       }
     }
-    for (var j = 0; j < board.length; j++) {
+    for (let j = 0; j < board.length; j++) {
       if (board[j] === board[j + 3] && board[j + 3] === board[j + 6]) {
         if (board[j] == player) {
-          return +10;
+          return +10
         } else if (board[j] == opponent) {
-          return -10;
+          return -10
         }
       }
     }
@@ -225,82 +224,79 @@ const aiMove = (() => {
       (board[4] == board[2] && board[4] == board[6])
     ) {
       if (board[4] == player) {
-        return +10;
+        return +10
       } else if (board[4] == opponent) {
-        return -10;
+        return -10
       }
     }
 
-    return 0;
+    return 0
   }
 
   function minimax(board, depth, isMax) {
-    const player = displayController.getCurrent();
-    const opponent = displayController.getCurrent() === "x" ? "o" : "x";
-    var score = evaluate(board);
+    const player = displayController.getCurrent()
+    const opponent = displayController.getCurrent() === 'x' ? 'o' : 'x'
+    const score = evaluate(board)
 
     if (score == 10) {
-      return score;
+      return score
     }
 
     if (score == -10) {
-      return score;
+      return score
     }
 
     if (!isMovesLeft(board)) {
-      return 0;
+      return 0
     }
 
     if (isMax) {
-      var best = -1000;
+      let best = -1000
 
-      for (var i = 0; i < board.length; i++) {
-        if (board[i] == "-") {
-          board.splice(i, 1, player);
-          var value = minimax(board, depth + 1, false);
-          best = Math.max(best, value);
-
-          board.splice(i, 1, "-");
+      for (let i = 0; i < board.length; i++) {
+        if (board[i] == '-') {
+          board.splice(i, 1, player)
+          const value = minimax(board, depth + 1, false)
+          best = Math.max(best, value)
+          board.splice(i, 1, '-')
         }
       }
-      return best;
+      return best
     } else if (!isMax) {
-      var best = 1000;
-      for (var i = 0; i < board.length; i++) {
-        if (board[i] == "-") {
-          board.splice(i, 1, opponent);
-          var value = minimax(board, depth + 1, true);
-          best = Math.min(best, value);
-
-          board.splice(i, 1, "-");
+      let best = 1000
+      for (let i = 0; i < board.length; i++) {
+        if (board[i] == '-') {
+          board.splice(i, 1, opponent)
+          const value = minimax(board, depth + 1, true)
+          best = Math.min(best, value)
+          board.splice(i, 1, '-')
         }
       }
-      return best;
+      return best
     }
   }
 
   function findBestMove(board) {
-    const player = displayController.getCurrent();
-    var bestVal = -1000;
-    var bestMove = -1;
+    const player = displayController.getCurrent()
+    let bestVal = -1000
+    let bestMove = -1
 
-    for (var i = 0; i < board.length; i++) {
-      if (board[i] == "-") {
-        board.splice(i, 1, player);
-        var moveVal = minimax(board, 0, false);
-
-        board.splice(i, 1, "-");
-
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] == '-') {
+        board.splice(i, 1, player)
+        const moveVal = minimax(board, 0, false)
+        board.splice(i, 1, '-')
         if (moveVal > bestVal) {
-          bestMove = i;
-          bestVal = moveVal;
+          bestMove = i
+          bestVal = moveVal
         }
       }
     }
-    return bestMove;
+    return bestMove
   }
 
   return {
     findBestMove,
-  };
-})();
+  }
+})()
+
